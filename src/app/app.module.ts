336ac85a412cf97750f66f9bf56f9cdcf4f516d5
A.Khattab <abdokhattab5@gmail.com>
2022-06-03 21:04:34 +0200

    Add Login Page UI Version 1

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,20 +1,30 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { FormsModule } from '@angular/forms';
+import {AuthModule} from "./auth/auth.module";
 
 import { AppComponent } from './app.component';
 import { SidebarHeaderComponent } from './compnents/sidebar-header/sidebar-header.component';
 import {AppRoutingModule} from "./app-routing-module";
 import {HomeComponent} from "./compnents/home/home.component";
+import {LoginComponent} from "./compnents/login/login.component";
 
 @NgModule({
   declarations: [
     AppComponent,
     SidebarHeaderComponent,
-    HomeComponent
+    HomeComponent,
+    LoginComponent
+
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    FormsModule,
+    AuthModule
+  ],
+  exports:[
+
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/app/compnents/login/login.component.ts
@@ -0,0 +1,33 @@
+import { Component, OnInit } from '@angular/core';
+import {Router} from '@angular/router'; // import router from angular router
+
+@Component({
+  selector: 'app-login',
+  templateUrl: './login.component.html',
+  styleUrls: ['./login.component.css']
+})
+export class LoginComponent implements OnInit {
+
+  email:string="";
+  password:string="";
+
+  constructor(private route:Router) { }
+
+  ngOnInit(): void {
+  }
+
+  loginUser()
+  {
+
+    if (this.email=="abdokhattab@gmail.com" && this.password=="admin1234")
+    {
+      this.route.navigate(['/home']);
+      console.log("welcome");
+    }
+    else{
+
+    }
+  }
+
+
+}
