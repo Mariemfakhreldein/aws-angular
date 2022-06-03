af86aa6229de9e6264c46ab3f99d60927b52619c
Mariem <mariem.mfe@gmail.com>
2022-06-03 14:17:17 +0200

    Add routing

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,18 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
 import { SidebarHeaderComponent } from './compnents/sidebar-header/sidebar-header.component';
+import {AppRoutingModule} from "./app-routing-module";
+import {HomeComponent} from "./compnents/home/home.component";
 
 @NgModule({
   declarations: [
     AppComponent,
-    SidebarHeaderComponent
+    SidebarHeaderComponent,
+    HomeComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    AppRoutingModule
   ],
   providers: [],
   bootstrap: [AppComponent]
