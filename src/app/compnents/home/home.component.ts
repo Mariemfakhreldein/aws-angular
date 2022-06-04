b8d4e07b220e8c9c5af2c5a7a746a13fa657fb22
salmaFayez <55198618+salmafayez@users.noreply.github.com>
2022-06-04 13:30:52 +0200

    display messages on success

--- src/app/compnents/home/home.component.ts
+++ src/app/compnents/home/home.component.ts
@@ -9,16 +9,20 @@ import {HttpClient, HttpHeaders} from "@angular/common/http";
 })
 export class HomeComponent implements OnInit {
 
-  branches = false;
-  templates = false;
+  admin = false;
+  hello = false;
+
+  adminMessage :string;
+  usersMessage :string;
+  helloMessage :string;
 
   constructor(private authService: AuthService, private http: HttpClient) { }
 
   ngOnInit(): void {
     if(this.authService.getPrivileges().includes("WRITE") ){
-      this.branches = true;
+      this.admin = true;
     }else if(this.authService.getPrivileges().includes("READ") ){
-      this.templates = true;
+      this.hello = true;
     }
   }
 
@@ -28,6 +32,9 @@ export class HomeComponent implements OnInit {
     console.log(headers);
     this.http.get<Response>("http://localhost:4545/api/admin", {headers:headers}).subscribe(
       (response)=>{
+        this.adminMessage =" welcome admin";
+        this.usersMessage ="";
+        this.helloMessage ="";
         console.log("success Admin", response);
       },(error)=>{
         console.log("fail Admin", error);
@@ -41,6 +48,9 @@ export class HomeComponent implements OnInit {
     console.log(headers);
     this.http.get<Response>("http://localhost:4545/api/users", {headers:headers}).subscribe(
       (response)=>{
+        this.usersMessage =" welcome users";
+        this.adminMessage ="";
+        this.helloMessage ="";
         console.log("success Users", response);
       },(error)=>{
         console.log("fail Users", error);
@@ -54,6 +64,10 @@ export class HomeComponent implements OnInit {
     console.log(headers);
     this.http.get<Response>("http://localhost:4545/api/hello", {headers:headers}).subscribe(
       (response)=>{
+        this.helloMessage =" welcome hello";
+        this.adminMessage ="";
+        this.usersMessage ="";
+
         console.log("success Hello", response);
       },(error)=>{
         console.log("fail Hello", error);
