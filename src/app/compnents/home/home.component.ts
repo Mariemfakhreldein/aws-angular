aecd8f9a29ae0b2b1e23b9cb760737957dde7c53
salmaFayez <55198618+salmafayez@users.noreply.github.com>
2022-06-04 13:22:35 +0200

    fix requests to api

--- src/app/compnents/home/home.component.ts
+++ src/app/compnents/home/home.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import {AuthService} from "../../services/auth.service";
+import {HttpClient, HttpHeaders} from "@angular/common/http";
 
 @Component({
   selector: 'app-home',
@@ -7,9 +9,56 @@ import { Component, OnInit } from '@angular/core';
 })
 export class HomeComponent implements OnInit {
 
-  constructor() { }
+  branches = false;
+  templates = false;
+
+  constructor(private authService: AuthService, private http: HttpClient) { }
 
   ngOnInit(): void {
+    if(this.authService.getPrivileges().includes("WRITE") ){
+      this.branches = true;
+    }else if(this.authService.getPrivileges().includes("READ") ){
+      this.templates = true;
+    }
+  }
+
+  onAdminClick(){
+    const headers = new HttpHeaders().append('Authorization', 'Bearer ' + this.authService.jwt)
+
+    console.log(headers);
+    this.http.get<Response>("http://localhost:4545/api/admin", {headers:headers}).subscribe(
+      (response)=>{
+        console.log("success Admin", response);
+      },(error)=>{
+        console.log("fail Admin", error);
+      }
+    )
+  }
+
+  onUsersClick(){
+    const headers = new HttpHeaders().append('Authorization', 'Bearer ' + this.authService.jwt)
+
+    console.log(headers);
+    this.http.get<Response>("http://localhost:4545/api/users", {headers:headers}).subscribe(
+      (response)=>{
+        console.log("success Users", response);
+      },(error)=>{
+        console.log("fail Users", error);
+      }
+    )
+  }
+
+  onHelloClick(){
+    const headers = new HttpHeaders().append('Authorization', 'Bearer ' + this.authService.jwt)
+
+    console.log(headers);
+    this.http.get<Response>("http://localhost:4545/api/hello", {headers:headers}).subscribe(
+      (response)=>{
+        console.log("success Hello", response);
+      },(error)=>{
+        console.log("fail Hello", error);
+      }
+    )
   }
 
 }
