1029f5d5ffbaf03e7c722c4f885fdc7b20da4d37
salmaFayez <55198618+salmafayez@users.noreply.github.com>
2022-06-05 12:56:19 +0200

    services structure complete

--- src/app/services/auth.service.ts
+++ src/app/services/auth.service.ts
@@ -1,12 +1,14 @@
 import {EventEmitter, Injectable, OnInit} from '@angular/core';
-import { Observable} from "rxjs";
+import {BehaviorSubject, Observable} from "rxjs";
 import {LoginModel} from "../models/login.view.model";
 import {HttpClient} from "@angular/common/http";
+import {ApiService} from "./api.service";
+import {TokenService} from "./token.service";
 
 @Injectable({
   providedIn: 'root'
 })
-export class AuthService  implements OnInit{
+export class AuthService  implements OnInit {
 
   isAuthenticated = false;
   authenticationResultEvent = new EventEmitter<boolean>() ;
@@ -14,41 +16,67 @@ export class AuthService  implements OnInit{
   headers:any;
   jwt:any;
 
-  constructor(private _http: HttpClient) { }
+  logged = new BehaviorSubject<boolean>(this.islogged());
+
+  constructor(private http: HttpClient, private api: ApiService, private tokenService: TokenService) {
+  }
 
   ngOnInit() {
     console.log("auth service initialized")
   }
 
+  login(model: LoginModel) {
+    return this.api.post("/api/login", model);
+  }
+
+  setToken(token: string) {
+    this.tokenService.setToken(token);
+  }
+
+  getToken(): string {
+    return this.tokenService.getToken();
+  }
+
+  islogged(): boolean {
+    let token = this.tokenService.getToken();
+    return token != null;
+  }
+
+  changeLoggedStatus(status: boolean): void {
+    this.logged.next(status);
+  }
+
+
   authenticate(loginModel: LoginModel) {
-   this.validateUser(loginModel).subscribe(
-     next =>{
+    this.validateUser(loginModel).subscribe(
+      next => {
         this.jwtObject = next;
         this.isAuthenticated = true;
+        const encodedPayload = this.jwtObject['jwt'];
+        this.jwt = encodedPayload;
+        localStorage.setItem('token', JSON.stringify(encodedPayload));
+
         this.authenticationResultEvent.emit(true);
-     },
-     error =>{
+      },
+      error => {
         this.isAuthenticated = false;
         this.authenticationResultEvent.emit(false);
-     }
-   )
+      }
+    )
   }
 
-  validateUser (loginModel: LoginModel):Observable<Response>{
+  validateUser(loginModel: LoginModel): Observable<Response> {
     console.log(loginModel);
-    return this._http.post<Response>("http://localhost:4545/api/login", loginModel);
+    return this.http.post<Response>("http://localhost:4545/api/login", loginModel);
 
   }
 
 
-  getPrivileges():Array<String>{
+  getPrivileges(): Array<String> {
     let privileges = new Array<String>();
-    if(this.jwtObject == null)
-        return privileges;
-
+    if (this.jwtObject == null)
+      return privileges;
     const encodedPayload = this.jwtObject['jwt'];
-    this.jwt = encodedPayload;
-    localStorage.setItem('token', JSON.stringify(encodedPayload));
     let payload = encodedPayload.split(".")[1];
     let decoded = atob(payload);
     return JSON.parse(decoded).privileges;
