cc2115cf49dc3a36567aa868b7df452f20258308
amiraElmergawy <amiraelmergawy@gmail.com>
2022-06-21 09:34:35 +0200

    Create training-program.service.ts

---
+++ src/app/services/training-program.service.ts
@@ -0,0 +1,17 @@
+import { Injectable } from '@angular/core';
+import { ApiService } from './api.service';
+import { TrainingProgram } from './../models/instances/training.program.model';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class TrainingProgramService {
+
+  constructor(private api: ApiService) { }
+
+  create(trainingProgram: TrainingProgram){
+    return this.api.post("/api/training-programs",trainingProgram );//??
+  }
+
+
+}
