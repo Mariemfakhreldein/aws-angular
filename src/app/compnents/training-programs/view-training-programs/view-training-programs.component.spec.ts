d6adf97b54b3f99ead1ea92dc62860434d02fe18
amiraElmergawy <amiraelmergawy@gmail.com>
2022-06-23 09:56:54 +0200

    Add training program view component

---
+++ src/app/compnents/training-programs/view-training-programs/view-training-programs.component.spec.ts
@@ -0,0 +1,25 @@
+import { ComponentFixture, TestBed } from '@angular/core/testing';
+
+import { ViewTrainingProgramsComponent } from './view-training-programs.component';
+
+describe('ViewTrainingProgramsComponent', () => {
+  let component: ViewTrainingProgramsComponent;
+  let fixture: ComponentFixture<ViewTrainingProgramsComponent>;
+
+  beforeEach(async () => {
+    await TestBed.configureTestingModule({
+      declarations: [ ViewTrainingProgramsComponent ]
+    })
+    .compileComponents();
+  });
+
+  beforeEach(() => {
+    fixture = TestBed.createComponent(ViewTrainingProgramsComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+});
