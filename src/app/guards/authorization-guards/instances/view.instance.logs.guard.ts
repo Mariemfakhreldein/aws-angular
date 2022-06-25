782a4b21ec665658ed21569497022f856c1dae5d
salmaFayez <55198618+salmafayez@users.noreply.github.com>
2022-06-26 00:57:00 +0200

    add view instance logs guard

---
+++ src/app/guards/authorization-guards/instances/view.instance.logs.guard.ts
@@ -0,0 +1,20 @@
+import { Injectable } from '@angular/core';
+import {ActivatedRouteSnapshot, CanActivate, Router, RouterStateSnapshot, UrlTree} from '@angular/router';
+import { Observable } from 'rxjs';
+import {AuthService} from "../../../services/auth.service";
+
+@Injectable({
+  providedIn: 'root'
+})
+export class StartStopViewInstanceGuard implements CanActivate {
+  constructor(private authService:AuthService, private router:Router){}
+
+  canActivate( route: ActivatedRouteSnapshot, state: RouterStateSnapshot): boolean {
+    let isAuthorized = this.authService.containPrivilege('VIEW_INSTANCE_LOGS');
+    if (!isAuthorized){
+      this.router.navigateByUrl("not-authorized")
+    }
+    return isAuthorized;
+  }
+
+}
