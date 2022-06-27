707ad02eaf53e9b07c41506d8fd66593a2329802
Sally <sallyelbanawany1998@gmail.com>
2022-06-27 04:01:45 +0200

    update edit intake

--- src/app/compnents/intakes/edit-intake/edit-intake.component.ts
+++ src/app/compnents/intakes/edit-intake/edit-intake.component.ts
@@ -66,7 +66,6 @@ export class EditIntakeComponent implements OnInit {
       this.intakePut.id = this.id;
       this.intakePut.intakeName = this.intakeName;
       this.intakePut.intakeDescription = this.intakeDescription;
-      this.intakePut.trainingProgramId = 1;
       this.updateIntake(this.intakePut);
     }
 
